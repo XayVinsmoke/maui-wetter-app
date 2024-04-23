d94571bde9c50ac2ddd0c88dfc5ea3f0f2092065
XayVinsmoke <s4legendex3@web.de>
2024-04-23 13:50:57 +0200

    createt a new Projekt

---
+++ MauiWetterApp/MauiWetterApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiWetterApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiWetterApp/MauiWetterApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiWetterApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
